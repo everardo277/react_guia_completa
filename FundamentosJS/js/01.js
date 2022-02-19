3653b521249de832d3bd7886b0dabf7dae898dda
everardo277 <rene.contrerasari@nube.unadmexico.mx>
2022-02-19 16:56:18 -0600

    fundamentos de Javascript

---
+++ FundamentosJS/js/01.js
@@ -0,0 +1,21 @@
+// Variables - let 
+let cliente = "Juan"
+let precioDescuento = 220 // camelCase
+
+// Re asignar
+cliente = 20
+cliente = true
+
+// Pueden iniciar sin un valor
+let precio
+
+precio = 1000
+
+console.log(precio)
+
+console.log(cliente)
+console.log(precioDescuento)
+
+let suma = precioDescuento + precio
+
+console.log(suma)
